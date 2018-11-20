8d3bc572027001ee501ff2191ece5586d40902c6
Yadu <yadhu.bmsce@gmail.com>
2018-11-20 23:20:31 +0100

    updated cover page, added summary and unrolling example

--- matlab_code/plot_channel_polarization.m
+++ matlab_code/plot_channel_polarization.m
@@ -1,4 +1,4 @@
-i = 9;
+i = 12;
 design_epsilon = 0.4;
 
 scatter(1,design_epsilon,5,'b','filled','MarkerFaceAlpha',0.5);
@@ -6,7 +6,7 @@ hold on;
 
 xtick_values = ones(1,i);
 
-for j = 1:i
+for j = 4:i
     channels = channel_polarization(j,design_epsilon);
     blockLength = 2^j * ones(1,2^j);
     xtick_values(1,j) = 2^j;
@@ -14,11 +14,14 @@ for j = 1:i
     hold on;
 end
 
-xlim([0,2^i]);
+xlim([16,2^i]);
 ylim([0,1]);
-ylabel('Erasure probablity');
-xlabel('Block-length');
-xticks(xtick_values(4:end));
-
+ylabel('Erasure probablity','FontSize',12);
+xlabel('Block-length','FontSize',12);
+xticks(xtick_values(6:end));
 xtickangle(90);
+
+xt = get(gca,'XTick');
+set(gca,'FontSize',8);
+
 hold off;
